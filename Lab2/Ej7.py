1c428eb0011864c8e4fc1b1f7c54de007ba9faeb
sergimol <sergimol@ucm.es>
2023-02-21 21:02:59 +0100

    Ya esta el 7

--- Lab2/Ej7.py
+++ Lab2/Ej7.py
@@ -12,13 +12,34 @@ CHUNK = 2048
 def osc(frec,dur,vol):
     return vol * np.sin(2*np.pi*np.arange(int(SRATE*dur))*frec/SRATE)
 
-notas = {
-    "A": 440,
-    "B": 493.8835,
-    "C": 523.251,
-    "D": 587.33,
-    "E": 659.255,
-    "F": 698.456,
-    "G": 783.991,
-}
+notas = "A.BC.D.EF.G.a.bc.d.ef.g"
 
+# abrimos stream
+stream = sd.OutputStream(samplerate=SRATE, 
+    blocksize=CHUNK,
+    channels=1)
+
+# arrancamos stream
+stream.start()
+
+# partitura
+part = [('G', 0.5), ('G', 0.5), ('a', 1), ('G', 1),
+('c', 1), ('b',2), ('G', 0.5), ('G', 0.5), ('a', 1), ('G', 1),
+('d', 1), ('c',2), ('G', 0.5), ('G',0.5), ('g', 1), ('e', 1),
+('c', 1), ('b', 1), ('a', 1), ('f', 0.5), ('f', 0.5), ('e', 1),
+('c', 1), ('d', 1),('c', 2)]
+
+i = 0
+while len(part) > i:
+    frec = 440 * (2**(notas.index(part[i][0])/12))
+    dur = part[i][1]
+
+    nota = osc(frec, dur, 1)
+    #silencio para separar las notas
+    nota = np.append(nota, np.zeros(5))
+
+    stream.write(np.float32(nota))
+    i += 1
+
+stream.stop()
+exit()
